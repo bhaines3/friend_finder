f599cc9e296acfc9f97afa0ba253030c8fb395bd
Brandon Haines <haines.brandon.91@gmail.com>
2018-02-08 13:45:41 -0700

    made friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+       "name":"Mario",
+       "photo":"https://vignette.wikia.nocookie.net/goanimate-v2/images/3/3e/Mario_Jumping.png/revision/latest?cb=20150326220654",
+       "scores":[]
+    },
+    {
+       "name":"Luigi",
+       "photo":"https://volumearchives.files.wordpress.com/2015/09/luigi-coins-new-super-mario-bros-2.png",
+       "scores":[]
+    },
+    {
+       "name":"Peach",
+       "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Peach_%28Super_Mario_3D_World%29.png/220px-Peach_%28Super_Mario_3D_World%29.png",
+       "scores":[]
+    },
+    {
+       "name":"Wario",
+       "photo":"https://vignette.wikia.nocookie.net/villains/images/c/c3/Sochi_2014_olympic_wario_3d_render_by_ratchetmario-d8j2g4e.png/revision/latest?cb=20170618212000",
+       "scores":[]
+    },
+     {
+        "name":"Bowser",
+        "photo":"https://vignette.wikia.nocookie.net/dragons/images/6/67/Koopa.jpg/revision/latest?cb=20170916172417",
+        "scores":[]
+     },
+       {
+       "name":"Toad",
+       "photo":"https://upload.wikimedia.org/wikipedia/en/d/d1/Toad_3D_Land.png",
+       "scores":[]
+    },
+     {
+        "name":"Yoshi",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/3/39/YoshiMarioParty9.png",
+        "scores":[]
+     }
+     {
+        "name":"Donkey Kong",
+        "photo":"https://vignette.wikia.nocookie.net/vsbattles/images/9/90/SSB4_-_Donkey_Kong_Artwork.png/revision/latest?cb=20151116211323",
+        "scores":[]
+     },
+       {
+       "name":"Boo",
+       "photo":"https://vignette.wikia.nocookie.net/mariokart/images/2/27/King_Boo_-_Mario_Kart_Wii.PNG/revision/latest?cb=20170507213155",
+       "scores":[]
+    },
+     {
+        "name":"Daisy",
+        "photo":"https://vignette.wikia.nocookie.net/p__/images/d/dc/MarioParty8DM.png/revision/latest?cb=20111207050451&path-prefix=protagonist",
+        "scores":[]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
