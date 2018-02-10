a7d9b00ab65b6871f249b225e5c8caa0e3d504d6
Brandon Haines <haines.brandon.91@gmail.com>
2018-02-10 07:14:06 -0700

    updated readMe and friends.js scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,52 +2,52 @@ var friends = [
     {
        "name":"Mario",
        "photo":"https://vignette.wikia.nocookie.net/goanimate-v2/images/3/3e/Mario_Jumping.png/revision/latest?cb=20150326220654",
-       "scores":[]
+       "scores":[4, 2, 1, 1, 3, 1, 5, 3, 4, 1]
     },
     {
        "name":"Luigi",
        "photo":"https://volumearchives.files.wordpress.com/2015/09/luigi-coins-new-super-mario-bros-2.png",
-       "scores":[]
+       "scores":[4, 4, 3, 3, 2, 4, 3, 4, 2]
     },
     {
        "name":"Peach",
        "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Peach_%28Super_Mario_3D_World%29.png/220px-Peach_%28Super_Mario_3D_World%29.png",
-       "scores":[]
+       "scores":[5, 5, 1, 1, 4, 4, 1, 4, 5, 4]
     },
     {
        "name":"Wario",
        "photo":"https://vignette.wikia.nocookie.net/villains/images/c/c3/Sochi_2014_olympic_wario_3d_render_by_ratchetmario-d8j2g4e.png/revision/latest?cb=20170618212000",
-       "scores":[]
+       "scores":[2, 1, 5, 4, 3, 1, 5, 4, 2, 2]
     },
      {
         "name":"Bowser",
         "photo":"https://vignette.wikia.nocookie.net/dragons/images/6/67/Koopa.jpg/revision/latest?cb=20170916172417",
-        "scores":[]
+        "scores":[1, 1, 5, 5, 2, 1, 5, 5, 1, 1]
      },
        {
        "name":"Toad",
        "photo":"https://upload.wikimedia.org/wikipedia/en/d/d1/Toad_3D_Land.png",
-       "scores":[]
+       "scores":[5, 5, 1, 1, 1, 4, 1, 2, 2, 4]
     },
      {
         "name":"Yoshi",
         "photo":"https://upload.wikimedia.org/wikipedia/en/3/39/YoshiMarioParty9.png",
-        "scores":[]
+        "scores":[4, 3, 2, 1, 4, 2, 2, 1, 4, 1]
      },
      {
         "name":"Donkey Kong",
         "photo":"https://vignette.wikia.nocookie.net/vsbattles/images/9/90/SSB4_-_Donkey_Kong_Artwork.png/revision/latest?cb=20151116211323",
-        "scores":[]
+        "scores":[3, 3, 4, 4, 3, 1, 4, 3, 3, 2]
      },
        {
        "name":"Boo",
        "photo":"https://vignette.wikia.nocookie.net/mariokart/images/2/27/King_Boo_-_Mario_Kart_Wii.PNG/revision/latest?cb=20170507213155",
-       "scores":[]
+       "scores":[1, 2, 5, 4, 3, 3, 5, 4, 1, 5]
     },
      {
         "name":"Daisy",
         "photo":"https://vignette.wikia.nocookie.net/p__/images/d/dc/MarioParty8DM.png/revision/latest?cb=20111207050451&path-prefix=protagonist",
-        "scores":[]
+        "scores":[5, 4, 2, 1, 4, 5, 3, 4, 4, 1]
      }
  
  ]
